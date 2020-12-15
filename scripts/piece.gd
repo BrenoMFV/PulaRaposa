e046ffdcd94650dfe861b88ac299051905015adb
BrenoMFV <brenomfv@gmail.com>
2020-12-15 20:04:30 -0300

    Designing a global state system for each piece and player

---
+++ scripts/piece.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var pos = Vector2.ZERO
+var selected = false 
+var color = "None"
+
+var at_home = false
+func ready():
+	pass
+
+func _process(delta):
+	pass
+
+func move(target):
+	pass
