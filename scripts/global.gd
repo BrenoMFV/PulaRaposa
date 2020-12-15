8a749b8517741bae5205d4c99a29754d0f3b8f29
BrenoMFV <66791036+BrenoMFV@users.noreply.github.com>
2020-12-15 20:55:15 -0300

    Add files via upload

---
+++ scripts/global.gd
@@ -0,0 +1,17 @@
+extends Node
+
+var selected_piece_pos = Vector2.ZERO
+var selected_piece_color = "None"
+var current_player_color = "None"
+var selection_blocked = false
+var game_over = false 
+var state = []
+
+func _ready():
+	pass
+
+func _input(event):
+#	if event is InputEventMouseButton and event.pressed:
+#		var pos = world_to_map(event.position)
+#		print(pos)
+		pass
